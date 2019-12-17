549995b45bcb6244de0cb785a441042252cf4765
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-12-17 14:09:01 -0700

    Add route to Login component

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -3,11 +3,13 @@ import './App.scss';
 import { Route } from 'react-router-dom';
 
 import MoviesContainer from '../MoviesContainer/MoviesContainer';
+import Login from '../Login/Login';
 
 const App = () => {
   return (
     <main className="App">
       <Route exact path='/' component={MoviesContainer} />
+      <Route exact path='/login' component={Login} />
     </main>
   );
 }
