d462e8e662f5186c186e22a5ecf2bb2540695e11
Ben Firth <bfirth93@gmail.com>
2019-12-17 11:43:18 -0700

    Fetch initial movie data

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
 
-function App() {
-  return (
-    <main className="App">
-      
-    </main>
-  );
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  componentDidMount() {
+    fetch('https://rancid-tomatillos.herokuapp.com/api/v1/movies')
+      .then(response => response.json())
+      .then(data => console.log(data.movies))
+  }
+
+  render() {
+    return (
+      <main className="App">
+        
+      </main>
+    );
+  }
 }
 
 export default App;
