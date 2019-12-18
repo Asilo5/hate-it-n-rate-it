c3bef49c653da6fede00749f273edabf5ec02f69
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-12-18 15:50:58 -0700

    Move className from section to link

--- src/Containers/MoviesDisplay/MoviesDisplay.js
+++ src/Containers/MoviesDisplay/MoviesDisplay.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 
 const MoviesDisplay = ({ average_rating, id,poster_path, title}) => {
     return (
-        <Link to={`/movies/${id}`} key={id}>
-            <section className='movies-display'>
+        <Link to={`/movies/${id}`} key={id} className='movies-display'>
+            <section>
                 <p>Avg. rating: {average_rating}</p>
                 <p>{title}</p>
                 <img src={poster_path} alt={title} />
