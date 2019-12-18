d3dd428888ac5817f67d58ad9222bea11d853605
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-12-17 19:59:06 -0700

    Import css and add classname to container

--- src/Containers/MoviesContainer/MoviesContainer.js
+++ src/Containers/MoviesContainer/MoviesContainer.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import MoviesDisplay from '../MoviesDisplay/MoviesDisplay';
+import './MoviesContainer.scss'
 
 const MoviesContainer = ({ movies }) => {
     let theMovie = movies.map((movie) => {
         return <MoviesDisplay key={movie.id} {...movie} /> 
     })
     return (
-        <section>
+        <section className='movies-container'>
            {theMovie}
         </section>
     )
