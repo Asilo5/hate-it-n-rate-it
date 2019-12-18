ff28395d8af086853208c8ac85cd5a6aad003850
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-12-18 15:55:41 -0700

    Add className to Movie

--- src/Containers/Movie/Movie.js
+++ src/Containers/Movie/Movie.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Movie = ({ average_rating, id, backdrop_path, overview, poster_path, release_date, title}) => {
     return (
-        <section>
+        <section className='movie'>
             <p>{average_rating}</p>
             <p>{title}</p>
             <p>{release_date}</p>
             <p>{overview}</p>
             <img src={poster_path} alt={title} />
             <p>{}</p>
-        </section>
+        </section> 
     )
 }
 
