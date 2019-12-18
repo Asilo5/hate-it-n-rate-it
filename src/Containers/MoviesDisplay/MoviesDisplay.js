5885da60f279ebe6c36165bf32c8106b2156e983
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-12-17 19:52:41 -0700

    Add Avg. rating to moveisDisplay card

--- src/Containers/MoviesDisplay/MoviesDisplay.js
+++ src/Containers/MoviesDisplay/MoviesDisplay.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MoviesDisplay = ({ average_rating, id, backdrop_path, overview, poster_path, release_date, title}) => {
     return (
         <section>
-            <p>{average_rating}</p>
+            <p>Avg. rating: {average_rating}</p>
             <p>{title}</p>
             <img src={poster_path} alt={title} />
         </section>
