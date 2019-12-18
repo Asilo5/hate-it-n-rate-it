cc364ec051ed40092287157ff9eba36fa60a084e
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-12-17 19:54:34 -0700

    Import css file and give classname to container to style

--- src/Containers/MoviesDisplay/MoviesDisplay.js
+++ src/Containers/MoviesDisplay/MoviesDisplay.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './MoviesDisplay.scss';
 
 const MoviesDisplay = ({ average_rating, id, backdrop_path, overview, poster_path, release_date, title}) => {
     return (
-        <section>
+        <section className='movies-display'>
             <p>Avg. rating: {average_rating}</p>
             <p>{title}</p>
             <img src={poster_path} alt={title} />
