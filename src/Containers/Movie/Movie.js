d8183dfef3629660a9706a19e82cfb91cc7aacad
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-12-19 15:50:31 -0700

    Add button back to movies

--- src/Containers/Movie/Movie.js
+++ src/Containers/Movie/Movie.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import './Movie.scss';
+import { NavLink } from 'react-router-dom';
 
 export const Movie = ({ average_rating, id, backdrop_path, overview, poster_path, release_date, title}) => {
     return ( 
-        <section className='movie'>
-            <section className='movie-images'>
-                <img className ='back-ground' src={backdrop_path} alt={title} />
-                <img className ='main-img' src={poster_path} alt={title} />
-            </section>
-            <section className='movie-info'>
-                <p className='title' >{title}</p>
-                <p>Out on: {release_date}</p>
-                <p>{overview}</p>
-                    <div className='rating'>
-                        <p>Avg. rating: {average_rating}</p>
-                        <p> Add your rating </p>
-                    </div>
-            </section>
-        </section> 
+        <section>
+            <NavLink to='/' className='back-button'>Back to Movies</NavLink>
+            <section className='movie'>
+                <section className='movie-images'>
+                    <img className ='back-ground' src={backdrop_path} alt={title} />
+                    <img className ='main-img' src={poster_path} alt={title} />
+                </section>
+                <section className='movie-info'>
+                    <p className='title' >{title}</p>
+                    <p>Out on: {release_date}</p>
+                    <p>{overview}</p>
+                        <div className='rating'>
+                            <p>Avg. rating: {average_rating}</p>
+                            <p> Add your rating </p>
+                        </div>
+                </section>
+            </section> 
+
+        </section>
     )
 }
 
