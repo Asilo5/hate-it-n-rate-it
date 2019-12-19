b5da53b28c82b23ba9ab31f5cb271acfc8b2a239
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-12-19 13:26:47 -0700

    Rearrange display of elements within the movie card

--- src/Containers/Movie/Movie.js
+++ src/Containers/Movie/Movie.js
@@ -4,14 +4,17 @@ import './Movie.scss';
 export const Movie = ({ average_rating, id, backdrop_path, overview, poster_path, release_date, title}) => {
     return (
         <section className='movie'>
-            <p>{title}</p>
+            <p className='title' >{title}</p>
+            <p>Out on: {release_date}</p>
             <img className ='back-ground' src={backdrop_path} alt={title} />
             <section className='movie-info'>
                 <img className ='main-img' src={poster_path} alt={title} />
-                <div>
-                    <p>Avg. rating: {average_rating}</p>
-                    <p>{release_date}</p>
+                <div className='overview-rating'>
                     <p>{overview}</p>
+                    <div className='rating'>
+                        <p>Avg. rating: {average_rating}</p>
+                        <p> Add your rating </p>
+                    </div>
                 </div>
             </section>
         </section> 
