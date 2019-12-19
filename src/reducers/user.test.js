e0af97552b9d807d9e9e4ee8c7580fc3c16a4843
Ben Firth <bfirth93@gmail.com>
2019-12-19 15:19:07 -0700

    Remove console.logs

--- src/reducers/user.test.js
+++ src/reducers/user.test.js
@@ -2,16 +2,12 @@ import { user } from './user';
 
 describe('iuser reducer', () => {
   it('should return the initial state', () => {
-    // setup
     const expected = {}
-    // execution
     const result = user(undefined, {})
-    // assertion
     expect(result).toEqual(expected);
   })
 
   it('should return the correct state when yuou pass in GET_USER for type of the action object', () => {
-    // setup
     const initialState = {};
     const action = {
       type: 'GET_USER',
@@ -23,14 +19,12 @@ describe('iuser reducer', () => {
         }
       }
     }
-    // execution
     const result = user(initialState, action);
     const expected = {
       id: 1, 
       name: "Alan", 
       email: "alan@turing.io"
     }
-    // assertion
     expect(result).toEqual(expected);
   })
 })
