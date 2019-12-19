f827c2f735981af0b5f31262063406812183e81c
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-12-19 15:42:21 -0700

    Change style of movie card

--- src/Containers/Movie/Movie.js
+++ src/Containers/Movie/Movie.js
@@ -2,20 +2,20 @@ import React from 'react';
 import './Movie.scss';
 
 export const Movie = ({ average_rating, id, backdrop_path, overview, poster_path, release_date, title}) => {
-    return (
+    return ( 
         <section className='movie'>
-            <p className='title' >{title}</p>
-            <p>Out on: {release_date}</p>
-            <img className ='back-ground' src={backdrop_path} alt={title} />
-            <section className='movie-info'>
+            <section className='movie-images'>
+                <img className ='back-ground' src={backdrop_path} alt={title} />
                 <img className ='main-img' src={poster_path} alt={title} />
-                <div className='overview-rating'>
-                    <p>{overview}</p>
+            </section>
+            <section className='movie-info'>
+                <p className='title' >{title}</p>
+                <p>Out on: {release_date}</p>
+                <p>{overview}</p>
                     <div className='rating'>
                         <p>Avg. rating: {average_rating}</p>
                         <p> Add your rating </p>
                     </div>
-                </div>
             </section>
         </section> 
     )
