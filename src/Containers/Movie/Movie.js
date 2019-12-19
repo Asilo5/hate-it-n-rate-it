553e41b8a47ce25eed9fb9b2da1f50367fcf41d6
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-12-19 13:08:27 -0700

    Add image height and position relative to images inside movie card

--- src/Containers/Movie/Movie.js
+++ src/Containers/Movie/Movie.js
@@ -4,13 +4,16 @@ import './Movie.scss';
 export const Movie = ({ average_rating, id, backdrop_path, overview, poster_path, release_date, title}) => {
     return (
         <section className='movie'>
-            <img src={poster_path} alt={title} />
-            <div>
-                <p>Avg. rating: {average_rating}</p>
-                <p>{title}</p>
-                <p>{release_date}</p>
-                <p>{overview}</p>
-            </div>
+            <p>{title}</p>
+            <img className ='back-ground' src={backdrop_path} alt={title} />
+            <section className='movie-info'>
+                <img className ='main-img' src={poster_path} alt={title} />
+                <div>
+                    <p>Avg. rating: {average_rating}</p>
+                    <p>{release_date}</p>
+                    <p>{overview}</p>
+                </div>
+            </section>
         </section> 
     )
 }
