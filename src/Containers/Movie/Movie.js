b80f07263090be6bac09c306d9ce35c9af4c12a0
Ben Firth <bfirth93@gmail.com>
2019-12-19 15:17:26 -0700

    Add movie testing

--- src/Containers/Movie/Movie.js
+++ src/Containers/Movie/Movie.js
@@ -4,12 +4,19 @@ import './Movie.scss';
 export const Movie = ({ average_rating, id, backdrop_path, overview, poster_path, release_date, title}) => {
     return (
         <section className='movie'>
-            <p>{average_rating}</p>
-            <p>{title}</p>
-            <p>{release_date}</p>
-            <p>{overview}</p>
-            <img src={poster_path} alt={title} />
-            <p>{}</p>
+            <p className='title' >{title}</p>
+            <p>Out on: {release_date}</p>
+            <img className ='back-ground' src={backdrop_path} alt={title} />
+            <section className='movie-info'>
+                <img className ='main-img' src={poster_path} alt={title} />
+                <div className='overview-rating'>
+                    <p>{overview}</p>
+                    <div className='rating'>
+                        <p>Avg. rating: {average_rating}</p>
+                        <p> Add your rating </p>
+                    </div>
+                </div>
+            </section>
         </section> 
     )
 }
