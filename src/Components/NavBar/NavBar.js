ecfc226670cbaeffdfb033954a0a5ec25aa272e9
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-12-19 14:42:45 -0700

    Remove height and width of button and change colour to black

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -5,7 +5,7 @@ import './NavBar.scss'
 
 export const NavBar = ({ user}) => {
   return (
-    <div className='NavBar'>
+    <div className='NavBar'> 
       {!user.name ?
       <NavLink className='nav_button' to='/login'> Sign In </NavLink> : 
       <button className='nav_button' onClick={() => window.location.reload()}> Sign Out </button>
