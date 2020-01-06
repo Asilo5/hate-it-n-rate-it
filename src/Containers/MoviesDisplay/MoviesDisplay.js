331311abb3441a725a0b202d1e3acf0c3b1de692
Ben Firth <bfirth93@gmail.com>
2020-01-06 15:01:30 -0700

    Add display user ratings

--- src/Containers/MoviesDisplay/MoviesDisplay.js
+++ src/Containers/MoviesDisplay/MoviesDisplay.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import './MoviesDisplay.scss';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const MoviesDisplay = ({ average_rating, id,poster_path, title}) => {
+
+export const MoviesDisplay = ({ average_rating, id, poster_path, title, userRatings, user}) => {
+    
+    const findRatedMovie = () => {
+        if (userRatings.length !== 0) {
+            return userRatings.find(movie => movie.movie_id === id)
+        }
+    }
+
     return (
         <Link to={`/movies/${id}`} key={id} className='movies-display'>
             <section>
                 <h3>{title}</h3>
                 <img src={poster_path} alt={title} />
+                {findRatedMovie() ? 
+                    <p>Your rating: {findRatedMovie().rating}</p>
+                :
+                    <p></p>
+                }
                 <p>Avg. rating: {average_rating.toFixed(2)}</p>
             </section>
         </Link> 
     )
 }
 
-export default MoviesDisplay;
+export const mapStateToProps = (state) => ({
+    movies: state.movies,
+    userRatings: state.userRatings,
+    user: state.user
+})
+
+export default connect(mapStateToProps)(MoviesDisplay);
