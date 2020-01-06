2803bd9d92d9f8b052a6ee865b1a54cfa5d2083b
Ben Firth <bfirth93@gmail.com>
2020-01-06 15:39:37 -0700

    Convert to class component

--- src/Containers/Movie/Movie.js
+++ src/Containers/Movie/Movie.js
@@ -1,38 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Movie.scss';
+import { connect } from 'react-redux';
 
-export const Movie = ({ average_rating, id, backdrop_path, overview, poster_path, release_date, title}) => {
-    return ( 
-        <section className='movie'>
-            <h2 className='title' >{title}</h2>
-            <h3>Out on: {release_date}</h3>
-            <p>Avg. rating: {average_rating.toFixed(2)}</p>
-            <img className ='back-ground' src={backdrop_path} alt={title} />
-            <section className='movie-info'>
-                <img className ='main-img' src={poster_path} alt={title} />
-                <div className='overview-rating'>
-                    <p>{overview}</p>
+
+export class Movie extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            rating: null
+        }
+    }
+
+    
+    findRatedMovie = () => {
+        if (this.props.userRatings.length !== 0) {
+            return this.props.userRatings.find(movie => movie.movie_id === this.props.id)
+        }
+    }
+    
+    render() {
+        const { average_rating, id, backdrop_path, overview, poster_path, release_date, title, userRatings} = this.props
+
+        return ( 
+            <section className='movie'>
+                <h2 className='title' >{title}</h2>
+                <h3>Out on: {release_date}</h3>
+                <p>Avg. rating: {average_rating.toFixed(2)}</p>
+                {this.findRatedMovie() ? 
+                    <p>Your rating: {this.findRatedMovie().rating}</p>
+                :
                     <div className='rating'>
-                        {/* <p>Avg. rating: {average_rating}</p> */}
                         <p> Add your rating </p>
-                        <select>
-                            <option value={1} >1</option>
-                            <option value={2} >2</option>
-                            <option value={3} >3</option>
-                            <option value={4} >4</option>
-                            <option value={5} >5</option>
-                            <option value={6} >6</option>
-                            <option value={7} >7</option>
-                            <option value={8} >8</option>
-                            <option value={9} >9</option>
-                            <option value={10} >10</option>
-                        </select>
+                        <form>
+                            <select>
+                                <option value={1} >1</option>
+                                <option value={2} >2</option>
+                                <option value={3} >3</option>
+                                <option value={4} >4</option>
+                                <option value={5} >5</option>
+                                <option value={6} >6</option>
+                                <option value={7} >7</option>
+                                <option value={8} >8</option>
+                                <option value={9} >9</option>
+                                <option value={10} >10</option>
+                            </select>
+                            <button>SUBMIT</button>
+                        </form>
                     </div>
-                </div>
+                }
+                <img className ='back-ground' src={backdrop_path} alt={title} />
+                <section className='movie-info'>
+                    <img className ='main-img' src={poster_path} alt={title} />
+                    <div className='overview-rating'>
+                        <p>{overview}</p>
+                    </div>
+                </section>
             </section>
-        </section>
-    )
-}
+        )
+    }
+} 
 
-export default Movie;
+// ({ average_rating, id, backdrop_path, overview, poster_path, release_date, title, userRatings}) => {
+// }
+
+export const mapStateToProps = (state) => ({
+    movies: state.movies,
+    userRatings: state.userRatings,
+    user: state.user
+})
+
+export default connect(mapStateToProps)(Movie);
 
