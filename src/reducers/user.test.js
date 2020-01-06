0cdeaf4885d3a36ba71afba5e2a7f690b8e1552d
Ben Firth <bfirth93@gmail.com>
2020-01-05 18:05:01 -0700

    Change getuser to setuser

--- src/reducers/user.test.js
+++ src/reducers/user.test.js
@@ -1,16 +1,16 @@
 import { user } from './user';
 
-describe('iuser reducer', () => {
+describe('user reducer', () => {
   it('should return the initial state', () => {
     const expected = {}
     const result = user(undefined, {})
     expect(result).toEqual(expected);
   })
 
-  it('should return the correct state when yuou pass in GET_USER for type of the action object', () => {
+  it('should return the correct state when yuou pass in SET_USER for type of the action object', () => {
     const initialState = {};
     const action = {
-      type: 'GET_USER',
+      type: 'SET_USER',
       user: {
         user: {
           id: 1, 
