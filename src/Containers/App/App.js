36c7f442932fd375a1e3c804525f42641bf825a3
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-01-07 15:50:41 -0700

    Add updated snapshot test for app

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -7,14 +7,15 @@ import { addMoviesData } from '../../actions/index';
 import Login from '../Login/Login';
 import Header from '../../Components/Header/Header';
 import Movie from '../Movie/Movie';
+import { getMovies } from '../../utils/apiCalls';
 
 
 export class App extends Component {
 
   componentDidMount() {
-    fetch('https://rancid-tomatillos.herokuapp.com/api/v1/movies')
-      .then(response => response.json())
+    getMovies()
       .then(data => this.props.addMoviesData(data))
+      .catch(err => console.log(err))
   }
 
   render() {
@@ -37,11 +38,11 @@ export class App extends Component {
   }
 }
 
-const mapDispatchToProps = dispatch => ({
+export const mapDispatchToProps = dispatch => ({
   addMoviesData: movies => dispatch( addMoviesData(movies))
 })
 
-const mapStateToProps = ({ movies }) => ({
+export const mapStateToProps = ({ movies }) => ({
    movies
 })
 
--- src/Containers/App/App.test.js
+++ src/Containers/App/App.test.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import { App } from './App';
 import { shallow } from 'enzyme';
+import { getMovies } from '../../utils/apiCalls'; 
+jest.mock('../../utils/apiCalls');
+
 
 describe('App', () => {
+
+  let wrapper;
+  let mockMovies = [
+    {
+      id: 1,
+      title: "Jumanji: The Next Level",
+      poster_path: "https://image.tmdb.org/t/p/original//l4iknLOenijaB85Zyb5SxH1gGz8.jpg",
+      backdrop_path: "https://image.tmdb.org/t/p/original//zTxHf9iIOCqRbxvl8W5QYKrsMLq.jpg",
+      release_date: "2019-12-04",
+      overview: "In Jumanji: The Next Level, the gang is back but the game has changed. As they return to rescue one of their own, the players will have to brave parts unknown from arid deserts to snowy mountains, to escape the world's most dangerous game.",
+      average_rating: 6
+    }
+  ]
+
+  beforeEach(() => {
+    getMovies.mockImplementation(() => {
+      return Promise.resolve(mockMovies)
+    });
+
+    wrapper = shallow(<App movies={mockMovies}
+                           addMoviesData={jest.fn()} />);
+ 
+  })
+
   it('should match the snapshot', () => {
-    const wrapper = shallow(<App />);
     expect(wrapper).toMatchSnapshot();
   })
+
+  it('should call getMovies after component is mounted', () => {
+    expect(getMovies).toHaveBeenCalled();
+  })
+
 })
