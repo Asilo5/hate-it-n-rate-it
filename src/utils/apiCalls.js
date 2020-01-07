c8d74973d8deb1ffee165c8a641d02222778d004
Ben Firth <bfirth93@gmail.com>
2020-01-06 17:11:37 -0700

    Create post for movie rating

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,26 +1,57 @@
 export const fetchUser = async (email, password) => {
   const userInfo = {email, password};
+
   const options = {
     method: 'POST',
     body: JSON.stringify(userInfo),
     headers: {'Content-Type': 'application/json'}
-  }
+  };
+
   const response = await fetch('https://rancid-tomatillos.herokuapp.com/api/v1/login', options);
+
   if (!response.ok) {
     const error = await response.json();
     throw new Error(error.message);
-  }
+  };
+
   const user = await response.json();
+
   return user;
 }
 
 export const fetchRatings = async () => {
   const response = await fetch('https://rancid-tomatillos.herokuapp.com/api/v1/users/5/ratings');
+
   if (!response.ok) {
     const error = await response.json();
     throw new Error(error.message);
-  }
+  };
+
   const movieRatings = await response.json();
-  console.log(movieRatings);
+
   return movieRatings;
+}
+
+export const addRatings = async (id, rating) => {
+  const movieRating = { 
+    movie_id: id, 
+    rating: rating
+  };
+
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(movieRating),
+    headers: {'Content-Type': 'application/json'}
+  };
+
+  const response = await fetch('https://rancid-tomatillos.herokuapp.com/api/v1/users/5/ratings', options);
+  console.log(response);
+
+  // if (!response.ok) {
+  //   const error = await response.json();
+  //   throw new Error(error.message);
+  // };
+
+  const newMovieRatings = await response.json();
+  return newMovieRatings;
 }
